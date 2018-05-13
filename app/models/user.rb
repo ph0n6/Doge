c8066b31d540cafa7534e15669183224510fd947
ph0n6 <ph0n6@hotmail.com>
2018-05-13 11:12:37 +0700

    Add associations and helper methods to the User model. Create relationship between the User and the Friend Model. Method help query the Friend records

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,38 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :posts, dependent: :destroy
+  
+  # Association for Friends
+  has_many: friends
+  has_many :all_received_friend_requests,  
+  class_name: "Friend", 
+  foreign_key: "friend_id"
+
+  has_many :accepted_sent_friend_requests, -> { where(friends: { accepted: true}) }, 
+            through: :friends, 
+            source: :friend
+
+  has_many :accepted_received_friend_requests, -> { where(friends: { accepted: true}) }, 
+            through: :all_received_friend_requests, 
+            source: :user
+
+  has_many :pending_received_friend_requests, ->  { where(friends: { accepted: false}) }, 
+            through: :all_received_friend_requests, 
+            source: :user
+  
+  
+  # gets all your friends
+  def all_active_friends
+    accepted_sent_friend_requests | accepted_received_friend_requests
+  end
+  
+  # gets your pending sent and received friends
+  def pending_friends
+    pending_sent_friend_requests | pending_received_friend_requests
+  end
+  
+  # gets a friend record
+  def friend(friend)
+  Friend.where(user_id: self.id, friend_id: friend.id)[0]
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,10 +3,87 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
   context 'Associations' do
+    # Post spec
     it 'has_many posts' do
       association = described_class.reflect_on_association(:posts)
       expect(association.macro).to eq :has_many
       expect(association.options[:dependent]).to eq :destroy
     end
+
+    # Friend Spec  
+    it 'has_many friends' do
+      association = described_class.reflect_on_association(:friends)
+      expect(association.macro).to eq :has_many
+    end
+  
+    it 'has_many all_received_friend_requests' do
+      association = described_class.reflect_on_association(:all_received_friend_requests)
+      expect(association.macro).to eq :has_many
+      expect(association.options[:class_name]).to eq 'friend'
+      expect(association.options[:foreign_key]).to eq 'friend_id'
+    end
+  
+    it 'has_many accepted_sent_friend_requests' do
+      association = described_class.reflect_on_association(:accepted_sent_friend_requests)
+      expect(association.macro).to eq :has_many
+      expect(association.options[:through]).to eq :friends
+      expect(association.options[:source]).to eq :friend
+    end
+  
+    it 'has_many accepted_received_friend_requests' do
+      association = described_class.reflect_on_association(:accepted_received_friend_requests)
+      expect(association.macro).to eq :has_many
+      expect(association.options[:through]).to eq :all_received_friend_requests
+      expect(association.options[:source]).to eq :user
+    end
+  
+    it 'has_many pending_sent_friend_requests' do
+      association = described_class.reflect_on_association(:pending_sent_friend_requests)
+      expect(association.macro).to eq :has_many
+      expect(association.options[:through]).to eq :friends
+      expect(association.options[:source]).to eq :friend
+    end
+  
+    it 'has_many pending_received_friend_requests' do
+      association = described_class.reflect_on_association(:pending_received_friend_requests)
+      expect(association.macro).to eq :has_many
+      expect(association.options[:through]).to eq :all_received_friend_requests
+      expect(association.options[:source]).to eq :user
+    end
+  end
+  
+  context 'Methods' do
+    let(:user) { build(:user) }
+    let(:friend_requests) do
+      @user = create(:user)
+      create_list(:friend, 2)
+      create_list(:friend, 2, accepted: true)
+      create(:friend, user_id: @user.id)
+      create(:friend, user_id: @user.id, accepted: true)
+      create(:friend, friend_id: @user.id)
+      create(:friend, friend_id: @user.id, accepted: true)
+    end
+  
+    it 'accepted_sent_friend_requests gets only accepted requests' do
+      friend_requests
+      expect(@user.accepted_sent_friend_requests.count).to eq 1
+    end
+  
+    it 'accepted_received_friend_requests gets only accepted requests' do
+      friend_requests
+      expect(@user.accepted_received_friend_requests.count).to eq 1
+    end
+  
+    it 'pending_sent_friend_requests gets only unaccepted requests' do
+      friend_requests
+      expect(@user.pending_sent_friend_requests.count).to eq 1
+    end
+  
+    it 'pending_received_friend_requests gets only unaccepted requests' do
+      friend_requests
+      expect(@user.pending_received_friend_requests.count).to eq 1
+  end
+
+
   end
 end
